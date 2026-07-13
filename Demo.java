1b310bbe2e33f195da086a2f57fa25da44f5b102
Your Name <you@example.com>
2026-07-13 09:25:00 +0530

    Demo.java change by developerproject

--- Demo.java
+++ Demo.java
@@ -3,5 +3,14 @@ class Demo{
 public static void main (String []args)
 {
 system.out.print("Hello");
+
+int a=10;
+int b=20;
+
+demo1 obj = new demo1;
+obj.a;
+obj.b;
+
+system.out.print("Done");
 }
 }
